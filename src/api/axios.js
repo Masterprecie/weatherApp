9e29c840e0b5d81f14a65ffef7ff341cb555490d
Precious <ikpaprecious2@gmail.com>
2023-11-09 03:08:13 -0800

    fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,4 @@
-import axios from 'axios'
+import axios from "axios";
 export default axios.create({
-	baseURL: 'https://api.openweathermap.org/data/2.5'
+  baseURL: "https://api.openweathermap.org/data/2.5",
 });
-
-
-
-// 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
